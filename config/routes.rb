a102f54e0b0db1df9a8c81f54f1bc8da392d1936
Doug Wright <mrdougwright@gmail.com>
2015-03-07 21:37:11 -0800

    add devise views; mess w/devise and styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'discoveries#claim'
 
-  devise_for :explorers
+  devise_for :explorers, path_names: {
+    sign_in: "login", sign_out: "logout", sign_up: "begin"
+  }
 
   resources :explorers
   resources :discoveries
